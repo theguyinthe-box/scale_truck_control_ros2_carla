9491fdf42bb0ddbbc54d6928ecfcb07d5bb2b30c
RazorRamon <brd@razorramon.sdsu.edu>
2024-02-01 18:16:32 -0800

    modded config files to include ackermann_msg in CARLA.

--- include/carla_control/CarlaControl.hpp
+++ include/carla_control/CarlaControl.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 //C++
-#include <iostream>
+/*#include <iostream>
 #include <pthread.h>
 #include <thread>
 #include <mutex>
@@ -14,11 +14,12 @@
 #include <memory>
 #include <fstream>
 #include <string>
-
+*/
 //ROS2
 #include "rclcpp/rclcpp.hpp"
 //#include "std_msgs/msg/int32.hpp"
 //#include "std_msgs/msg/string.hpp"
+#include "ackermann_msgs/msg/AckermannDrive.hpp"
 
 //custom msgs
 #include "ros2_msg/msg/lrc2ocr.hpp"
@@ -31,9 +32,9 @@ namespace scale_truck_control{
 class CarlaControl : public rclcpp::Node
 {
 public:
-    LocalRC();
+    CarlaControl();
 
-    ~LocalRC();
+    ~CarlaControl();
 
 private:
     void init();
@@ -42,45 +43,14 @@ private:
     rclcpp::Subscription<ros2_msg::msg::Lrc2ocr>::SharedPtr LrcSubscriber_;
 
     //Publisher
-    //rclcpp::Publisher<ros2_msg::msg::Lrc2xav>::SharedPtr FVPublisher_;
-    //TODO: Carla ackermann message publisher
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>::SharedPtr CarlaPublisher_;
 
     //Callback
-    void Lrc2ocrCallback(const ros2_msg::msg::Lrc2ocr::SharedPtr msg);
+    void LrcCallback(const ros2_msg::msg::Lrc2ocr::SharedPtr msg);
 
-    bool isNodeRunning();
-    void rosPub();
-    void recordData(struct timeval *startTime);
-    void printStatus();
-    void communicate();
-    void radio();
-
-    int index_;
-    bool isNodeRunning_;
-    bool EnableConsoleOutput_;
-    std::string log_path_;
-    float a_, b_, l_;
-    float epsilon_;
-    float rotation_angle_ = 0.0f;
-    float lateral_offset_ = 0.0f;
-
-    float angle_degree_ = 0;
-    float cur_dist_ = 0.8f;
-    float tar_dist_ = 0.8f;
-    float ref_vel_ = 0;
-    float cur_vel_ = 0;
     float tar_vel_ = 0;
-    float est_vel_ = 0;
-    float hat_vel_ = 0;
-    float sat_vel_ = 0;
-    double time_ = 0;
-    double req_time_ = 0;
-
-    std::thread lrcThread_;
-    std::thread udpThread_;
-    std::thread tcpThread_;
+    float steer_angle_ = 0;
     std::mutex data_mutex_;
-    std::mutex time_mutex_;
 };
 
 }
--- launch/LV_carla.launch.py
+++ launch/LV_carla.launch.py
@@ -12,24 +12,6 @@ from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
 
-    ###############
-    # Lidar param #
-    ###############
-#    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
-#    serial_baudrate = LaunchConfiguration('serial_baudrate', default='1000000') 
-#    frame_id = LaunchConfiguration('frame_id', default='laser')
-#    inverted = LaunchConfiguration('inverted', default='false')
-#    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-#    scan_mode=LaunchConfiguration('scan_mode', default='DenseBoost')#Standard,DenseBoost
-
-    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
-    serial_baudrate = LaunchConfiguration('serial_baudrate', default='256000') #for A3 is 256000
-    frame_id = LaunchConfiguration('frame_id', default='laser')
-    inverted = LaunchConfiguration('inverted', default='false')
-    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
-    scan_mode = LaunchConfiguration('scan_mode', default='Stability')
-
-
     ros_param_file = os.path.join(
             get_package_share_directory('scale_truck_control_ros2'), 
             'config', 
@@ -54,19 +36,6 @@ def generate_launch_description():
 #                        ])],
 #            remappings=[('image_raw', 'usb_cam/image_raw')],
 #            output='screen')
-
-#    rplidarS2_node=Node(
-#            package='rplidar_ros2',
-#            namespace='LV',
-#            executable='rplidar_scan_publisher',
-#            name='rplidar_scan_publisher',
-#            parameters=[{'serial_port': serial_port, 
-#                         'serial_baudrate': serial_baudrate, 
-#                         'frame_id': frame_id,
-#                         'inverted': inverted, 
-#                         'angle_compensate': angle_compensate,
-#                         'scan_mode':scan_mode}],
-#            output='screen')         
 
 ### TODO: instead of launching lidar, remap Carla lidar topic
 #    rplidarA3_node=Node(
@@ -135,10 +104,9 @@ def generate_launch_description():
 
     ld = LaunchDescription()
     
-    ld.add_action(usb_cam_node)
+    #ld.add_action(usb_cam_node)
     ld.add_action(lane_detection_node)
-    ld.add_action(rplidarA3_node)
-#    ld.add_action(rplidarS2_node)
+    #ld.add_action(rplidarA3_node)
     ld.add_action(laserfilter_node)
     ld.add_action(object_node)
     ld.add_action(control_node)
